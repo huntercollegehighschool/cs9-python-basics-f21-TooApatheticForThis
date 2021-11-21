2fd9dfd326f2f31ca6745b07988c3436ab91bd6f
TooApatheticForThis <93940122+TooApatheticForThis@users.noreply.github.com>
2021-11-21 18:57:45 -0500

    Update part1.py
    I added more number variables

--- part1.py
+++ part1.py
@@ -15,3 +15,4 @@ fourth_value = int(input("Enter a fourth number: "))
 fifth_value = int(input("Enter a fifth number: "))
 
 print("The sum of the numbers you entered is", first_value + second_value + third_value + fourth_value + fifth_value)
+print("hello")
